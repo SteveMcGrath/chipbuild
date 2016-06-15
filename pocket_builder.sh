89fd0a0cb413122b9b2cd0351ba473515f2250bf
Steven McGrath <smcgrath@tenable.com>
2016-06-15 14:04:38 -0500

    sshon and sshoff should be sudo.  added getip and removed pocket-home replacement question.

--- pocket_builder.sh
+++ pocket_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # PocketCHIP Builder Script
-# Build 1
+# Build 2
 
 BROWSER="dwb"
 REPO="https://raw.githubusercontent.com/SteveMcGrath/chipbuild/master"
@@ -50,30 +50,31 @@ fi
 # If there isnt an aliases.sh file, then we will create one with some defaults added in.
 if [ ! -f "$HOME/.profile_scripts/aliases.sh" ];then
 	echo "[*] Creating default aliases file"
-	echo 'alias sshon="systemctl start ssh"' > $HOME/.profile_scripts/aliases.sh
-	echo 'alias sshoff="systemctl stop ssh"' >> $HOME/.profile_scripts/aliases.sh
+	echo 'alias sshon="sudo systemctl start ssh"' > $HOME/.profile_scripts/aliases.sh
+	echo 'alias sshoff="sudo systemctl stop ssh"' >> $HOME/.profile_scripts/aliases.sh
+	echo "alias getip=\"ip addr | awk '/inet/ {print $2}'\"" >> $HOME/.profile_scripts/aliases.sh
 fi
 
 # Now to replace the pocket-home configuration file with our templated one and to
 # replace the Help icon with a web browser.
-echo '*****************************************'
-echo 'We can switch out the help file icon'
-echo 'with the bworser icon and set the browser'
-echo "to be $BROWSER.  Please note that this"
-echo 'will overwrite any other changes you may'
-echo 'have made to the home screen.'
-echo '*****************************************'
-echo -en 'Replace Help with Browser? (y/n)>';read answer
+echo '*********************************************'
+echo '* We will switch out the help file icon     *'
+echo '* with the bworser icon and set the browser *'
+echo "* to be $BROWSER.  Please note that this    *"
+echo '* will overwrite any other changes you may  *'
+echo '* have made to the home screen.  The file   *'
+echo '* currenthle installed will be located at:  *'
+echo '*  /usr/share/pocket-home/config-orig.json  *'
+echo '*********************************************'
 
-if [ "$answer" == "y" ];then
-	# Lets move the original configuration somewhere and then pull down the
-	# new template.  From there we need to replace the temnplated value with
-	# the browser that we will be using.
-	mv /usr/share/pocket-home/config.json /usr/share/pocket-home/config-orig.json
-	sudo wget -O /usr/share/pocket-home/config.json $REPO/configs/pocket-home-template.json
-	sudo sed -i "s/BROWSER_EXEC/${BROWSER}/g" /usr/share/pocket-home/config.json
+# Lets move the original configuration somewhere and then pull down the
+# new template.  From there we need to replace the temnplated value with
+# the browser that we will be using.
+echo '[*] Replacing the default pocket-home config'
+mv /usr/share/pocket-home/config.json /usr/share/pocket-home/config-orig.json
+sudo wget -O /usr/share/pocket-home/config.json $REPO/configs/pocket-home-template.json
+sudo sed -i "s/BROWSER_EXEC/${BROWSER}/g" /usr/share/pocket-home/config.json
 
-	# Now that the new config file is written, lets go ahead and bounce the
-	# pocket-home service.
-	sudo skill pocket-home 
-fi
+# Now that the new config file is written, lets go ahead and bounce the
+# pocket-home service.
+sudo skill pocket-home 
