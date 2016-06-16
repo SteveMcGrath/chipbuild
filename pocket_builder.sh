3c4e1891acc873f7581b1c2282764ba8b4cbec6a
Steven McGrath <smcgrath@tenable.com>
2016-06-16 10:32:36 -0500

    Converted the tabs to spaces for readability.

--- pocket_builder.sh
+++ pocket_builder.sh
@@ -10,11 +10,11 @@ sudo apt-get update
 
 # Now lets install the software that we want.
 sudo apt-get -y install openssh-client\
-						openssh-server\
-						irssi\
-						dnsutils\
-						moc\
-					    $BROWSER
+                        openssh-server\
+                        irssi\
+                        dnsutils\
+                        moc\
+                        $BROWSER
 
 # Now lets disable the SSH service.  We don't want this
 # firing up unless we tell it to.
@@ -24,43 +24,44 @@ sudo systemctl stop ssh
 # If the browser is set to dwb, then we will want to
 # setup the configuration file for dwb.
 if [ "$BROWSER" == "dwb" ];then
-	
-	# If the directory doesn't exist, then lets create it.
-	if [ ! -d "$HOME/.config/dwb" ];then
-		mkdir -p $HOME/.config/dwb
-	fi
+    
+    # If the directory doesn't exist, then lets create it.
+    if [ ! -d "$HOME/.config/dwb" ];then
+        mkdir -p $HOME/.config/dwb
+    fi
 
-	# Next lets go ahead and download the dwb config file
-	# that seems to work well with the PocketCHIP
-	wget -qO ~/.config/dwb/settings $REPO/configs/dwb-settings
+    # Next lets go ahead and download the dwb config file
+    # that seems to work well with the PocketCHIP
+    wget -qO ~/.config/dwb/settings $REPO/configs/dwb-settings
 fi
 
 # To keep things nice and clean, we will build out a profile_scripts
 # directory and source these files into the user's prompt.
 if [ ! -d "$HOME/.profile_scripts" ];then
-	echo "[*] Creating Profile Scripts Directory and Shimming $USER .bashrc"
-	mkdir -p $HOME/.profile_scripts
+    echo "[*] Creating Profile Scripts Directory and Shimming $USER .bashrc"
+    mkdir -p $HOME/.profile_scripts
 
-	echo '' >> $HOME/.bashrc
-	echo '# This is a shim to allow for multiple profile script to exist in the' >> $HOME/.bashrc
-	echo '# user home.  All of the shell scripts in ~/.profile_scripts will be' >> $HOME/.bashrc
-	echo '# loaded into the user environment.' >> $HOME/.bashrc
-	echo 'for SCRIPT_NAME in $(ls $HOME/.profile_scripts/*.sh);do' >> $HOME/.bashrc
-	echo '    source $SCRIPT_NAME' >> $HOME/.bashrc
-	echo 'done' >> $HOME/.bashrc
-	echo 'unset SCRIPT_NAME' >> $HOME/.bashrc
+    # This is the addition that we are adding into the users .bashrc.
+    echo ''                                                                         >> $HOME/.bashrc
+    echo '# This is a shim to allow for multiple profile script to exist in the'    >> $HOME/.bashrc
+    echo '# user home.  All of the shell scripts in ~/.profile_scripts will be'     >> $HOME/.bashrc
+    echo '# loaded into the user environment.'                                      >> $HOME/.bashrc
+    echo 'for SCRIPT_NAME in $(ls $HOME/.profile_scripts/*.sh);do'                  >> $HOME/.bashrc
+    echo '    source $SCRIPT_NAME'                                                  >> $HOME/.bashrc
+    echo 'done'                                                                     >> $HOME/.bashrc
+    echo 'unset SCRIPT_NAME'                                                        >> $HOME/.bashrc
 fi
 
 # If there isnt an aliases.sh file, then we will create one with some defaults added in.
 if [ ! -f "$HOME/.profile_scripts/aliases.sh" ];then
-	ALIASES=$HOME/.profile_scripts/aliases.sh
-	echo "[*] Creating default aliases file"
-	echo 'alias sshon="sudo systemctl start ssh"' 				>  $ALIASES
-	echo 'alias sshoff="sudo systemctl stop ssh"' 				>> $ALIASES
-	echo "alias getip=\"ip addr | awk '/inet/ {print $2}'\"" 	>> $ALIASES
-	echo 'alias ll="ls -al"'									>> $ALIASES
-	echo 'alias pgr="ps -ef | grep"'							>> $ALIASES
-	echo 'alias sag="sudo apt-get install"'						>> $ALIASES
+    ALIASES=$HOME/.profile_scripts/aliases.sh
+    echo "[*] Creating default aliases file"
+    echo 'alias sshon="sudo systemctl start ssh"'               >  $ALIASES
+    echo 'alias sshoff="sudo systemctl stop ssh"'               >> $ALIASES
+    echo "alias getip=\"ip addr | awk '/inet/ {print $2}'\""    >> $ALIASES
+    echo 'alias ll="ls -al"'                                    >> $ALIASES
+    echo 'alias pgr="ps -ef | grep"'                            >> $ALIASES
+    echo 'alias sag="sudo apt-get install"'                     >> $ALIASES
 fi
 
 # Now to replace the pocket-home configuration file with our templated one and to
@@ -79,8 +80,13 @@ echo '*********************************************'
 # new template.  From there we need to replace the temnplated value with
 # the browser that we will be using.
 echo '[*] Replacing the default pocket-home config'
+
+# We only want to create the config-orig.json file if it doesnt already exist.
+# this stops us from plowing over the original config file multiple times.
 if [ ! -f /usr/share/pocket-home/config-orig.json ];then
-	sudo mv /usr/share/pocket-home/config.json /usr/share/pocket-home/config-orig.json
+    sudo mv /usr/share/pocket-home/config.json /usr/share/pocket-home/config-orig.json
+else
+    sudo mv /usr/share/pocket-home/config.json /usr/share/pocket-home/config-bak.json
 fi
 sudo wget -qO /usr/share/pocket-home/config.json $REPO/configs/pocket-home-template.json
 sudo sed -i "s/BROWSER_EXEC/${BROWSER}/g" /usr/share/pocket-home/config.json
